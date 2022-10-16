5649a261a41859ee65c0de15181890ddce7ae439
July Rocha <nrochajuly@gmail.com>
2022-10-15 21:47:01 -0300

    :sparkles: Add new scene and loop btween scenes

---
+++ Assets/Scripts/CollisonHandler.cs
@@ -0,0 +1,43 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class CollisonHandler : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("This thing is friendly");
+                break;
+            case "Finish":
+                LoadNextLevel();
+                break;
+            case "Fuel":
+                Debug.Log("Add Fuel");
+                break;
+            default:
+                ReloadLevel();
+                break;
+
+
+        }
+    }
+
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if(nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+}
